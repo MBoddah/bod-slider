88fea0704b33b25e443af7c274de9955d5d82fc0
Dmitry Morozov <mboddah@yandex.ru>
2021-04-30 21:48:42 +0300

    add scroll, scope and turn properties

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 'use strict'
 
 function slider( {
-    selectors,   // Object of selectors Required: slide, container
+    selectors,   // Object of selectors Required: slide, container.
     slidesList,  // Array of slide objects. Required properties: imgPath, alt.
-    startSlideIndex,  //Index of start slide
-    activateNavigationDots, // Expects true for creating navigation dots
-    activateLoop,
-    actuvateAutoTurning,
-    activateLinks,
-    minimizedWidth,
-    minimizedHealth,
-    maximizedWidth,
-    maximizedHeight,
-    turningInterval,
-    loadSpinnerImg,
+    startSlideIndex,  // Index of start slide. Default: 1.
+    activateNavigationDots, // Expects true for creating navigation dots.
+    activateAutoTurning,  // Expexts true for activating autoturning. Works while arrow havent touched.
+    turningInterval,  // Auto turn timeout. Default: 10000.
+    activateSlidesMoving, // Expects true for auto scrolling elongated imgs and scoping others.
+    autoScrollSpeed, // Expects auto img scroll speed. Recomended speed: 1-5. Default: 2.
+    autoScopeSpeed, //
     leftArrowImg,
-    rightArrowImg,
-    dotsPosition,
-    arrowPosition,
-    dotsOverlay,
-    arrowsOverlay,
-    overlayOpacity}) {
+    rightArrowImg}) {
 
     const bodSelectors = prepareSelectors(selectors);           // Creates a selectors object. Declares standard classes to unspecified
     class sliderItem {                                          // Creates a slider object. Expects objects of img path (required), alt (required) and text
@@ -45,7 +36,6 @@ function slider( {
                 item.innerHTML = `
                 <img src=${this.imgSrc} alt=${this.alt}>
             `}
-
             this.parent.append(item);
         }
     }
@@ -54,14 +44,14 @@ function slider( {
         new sliderItem(slide).render();
     });
 
-    const slider = document.querySelector(bodSelectors.sliderSelector),
+    const slider = document.querySelector(bodSelectors.sliderSelector),         // Inits slider
         slides = document.querySelectorAll(bodSelectors.slidesSelector),
         slidesWrapper = document.querySelector(bodSelectors.wrapperSelector) || createSliderElement(slider, bodSelectors.wrapperSelector),
         sliderInner = document.querySelector(bodSelectors.innerSelector) || createSliderElement(slidesWrapper, bodSelectors.innerSelector),
         arrowPrev = document.querySelector(bodSelectors.prevArrowSelector) || createSliderElement(slidesWrapper, bodSelectors.prevArrowSelector),
         arrowNext = document.querySelector(bodSelectors.nextArrowSelector) || createSliderElement(slidesWrapper , bodSelectors.nextArrowSelector);
 
-    sliderInner.style.width = slides.length * 100 + '%';        // Inits slider
+    sliderInner.style.width = slides.length * 100 + '%';        
     sliderInner.style.display = 'flex';
     sliderInner.style.transition = '.5s all';
     slidesWrapper.style.overflow = 'hidden';
@@ -70,21 +60,60 @@ function slider( {
     const width = window.getComputedStyle(slidesWrapper).width,
         indicators = [];
     let offset = width.replace(/\D/g, '')*currentSlide;
-
-    sliderInner.style.transform = `translateX(-${offset}px)`;
+    let timerTurn;
 
     renderArrows(leftArrowImg, rightArrowImg, arrowPrev, arrowNext);
 
-    if (activateNavigationDots === true) {
-        createDots(slides, indicators, slider);
+    if (activateNavigationDots) {
+        if (activateNavigationDots === true) {                  // Inits selected options
+            createDots(slides, indicators, slider);
+        } else {
+            warnAboutUnexpectedValue('activateNavigationDots');
+        }
     }
 
-    arrowPrev.addEventListener('click', () =>{
+    if (activateSlidesMoving) {
+        if (activateSlidesMoving === true) {
+            document.querySelectorAll('.slider__slide img').forEach( slide => {
+                slide.onload = function () {
+                    if (slide.offsetHeight > slide.parentNode.offsetHeight * 1.5) {
+                        scrollImg(slide, autoScrollSpeed || 2)
+                    } else {
+                        slide.style.height = '100%';
+                        scopeImg(slide, autoScopeSpeed || 2)
+                    }
+                }
+            })
+        } else {
+            warnAboutUnexpectedValue('activateSlidesMoving');
+        }
+    }
+
+    if (activateAutoTurning) {
+        if (activateAutoTurning === true) {
+            const time = turningInterval || 10000;
+            timerTurn = setInterval( () => {
+                [currentSlide, offset] = showSlide(slides, ++currentSlide, offset, width , sliderInner, indicators)
+            }, time);
+        } else {
+            warnAboutUnexpectedValue('actiateAutoTurning')
+        }
+    }
+    
+    [currentSlide, offset] = showSlide(slides, currentSlide, offset, width , sliderInner, indicators)      // Shows start slide
+
+    arrowPrev.addEventListener('click', () =>{                                                             // Activates slider controls
         [currentSlide, offset] = showSlide(slides, --currentSlide, offset, width , sliderInner, indicators)
+        if (activateAutoTurning === true) {
+            clearInterval(timerTurn);
+        }
     })
 
     arrowNext.addEventListener('click', () =>{
         [currentSlide, offset] = showSlide(slides, ++currentSlide, offset, width , sliderInner, indicators)
+        if (activateAutoTurning === true) {
+            clearInterval(timerTurn);
+        }
     })
 
     
@@ -163,6 +192,25 @@ function slider( {
 
         return [slideIndex, offset];
     }
+
+    function scrollImg(img, speed) {
+        let offset = 0;
+        const rollup = setInterval( () => {
+            img.style.transform = `translateY(${(-offset)}px)`;
+            offset = offset + 0.2;
+
+            if (offset > (img.offsetHeight - img.parentNode.offsetHeight)) clearInterval(rollup)
+        }, 100/speed);
+    }
+
+    function scopeImg(img, speed) {
+        img.style.transform = 'scale(1.2)';
+        img.style.transition = `all ${60/speed}s`;
+    }
+
+    function warnAboutUnexpectedValue(value) {
+        console.warn(`Wrong property of ${value}`)
+    }
 }
 
 const selectors = {
@@ -174,7 +222,7 @@ const selectors = {
 
 const slidesList = [
     {imgSrc:'img/slider/pepper.jpg', alt: 'pepper'},
-    {imgSrc:'img/slider/food-12.jpg', alt: 'food'},
+    {imgSrc:'img/slider/food-12.png', alt: 'food'},
     {imgSrc:'img/slider/olive-oil.jpg', alt: 'oil', text: 'Пилот 1'},
     {imgSrc:'img/slider/paprika.jpg', alt: 'paprika'}
 ]
@@ -182,8 +230,13 @@ const slidesList = [
 slider({
     selectors, 
     slidesList,
-    leftArrowImg: 'icons/left.svg',
-    rightArrowImg: 'icons/right.svg',
+    leftArrowImg: 'icons/arrow-left.png',
+    rightArrowImg: 'icons/arrow-right.png',
     startSlideIndex: 2,
-    activateNavigationDots: true
+    activateNavigationDots: true,
+    activateAutoTurning: true,
+    turningInterval: 2000,
+    activateSlidesMoving: true,
+    autoScrollSpeed: 2,
+    autoScopeSpeed: 2
 });
